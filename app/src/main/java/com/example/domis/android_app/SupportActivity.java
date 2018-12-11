cd22ee7ff2476568d3610a4e4827ebd965a941a4
Dominik Wiecek <domis.wiecek@gmail.com>
2018-12-11 14:15:46 +0000

    uofsd

--- app/src/main/java/com/example/domis/android_app/SupportActivity.java
+++ app/src/main/java/com/example/domis/android_app/SupportActivity.java
@@ -2,12 +2,27 @@ package com.example.domis.android_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class SupportActivity extends AppCompatActivity {
+
+    private Button bookingsButton;
+    private Button suppButton;
+    private Button logoutButton;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+
+        bookingsButton = findViewById(R.id.bookingButton);
+        suppButton = findViewById(R.id.suppButton);
+        logoutButton = findViewById(R.id.logoutButton);
+        listView = findViewById(R.id.listView);
+
+
+
     }
 }
--- app/src/main/java/com/example/domis/android_app/activity/MenuActivity.java
+++ app/src/main/java/com/example/domis/android_app/activity/MenuActivity.java
@@ -3,26 +3,21 @@ package com.example.domis.android_app.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.domis.android_app.R;
 
 
 public class MenuActivity extends AppCompatActivity {
 
-    private Button bookingsButton;
-    private Button suppButton;
-    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        bookingsButton = findViewById(R.id.bookingButton);
-        suppButton = findViewById(R.id.suppButton);
-        logoutButton = findViewById(R.id.logoutButton);
 
-        
+
 
     }
 }
