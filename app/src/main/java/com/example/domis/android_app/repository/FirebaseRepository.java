42b3acec86865de78802a782b54fa268f0d2f04d
Dominik Wiecek <domis.wiecek@gmail.com>
2018-12-11 15:14:51 +0000

    uofsd

--- app/src/main/java/com/example/domis/android_app/repository/FirebaseRepository.java
+++ app/src/main/java/com/example/domis/android_app/repository/FirebaseRepository.java
@@ -22,8 +22,13 @@ import com.google.firebase.iid.InstanceIdResult;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
-import model.Entity;
+import com.example.domis.android_app.model.Entity;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
@@ -60,34 +65,33 @@ public class FirebaseRepository {
 
             }
         });*/
+    }
 
-        public <T extends Entity> List<T> getObjectList(String reference, Class<T> c) {
-            final Waiter waiter = new Waiter();
+    public <T extends Entity> List<T> getObjectList(String reference, Class<T> c) {
+        final Waiter waiter = new Waiter();
 
-            ref.child(reference).addListenerForSingleValueEvent(new ValueEventListener() {
+        myRef.child(reference).addListenerForSingleValueEvent(new ValueEventListener() {
 
-                @Override
-                public void onDataChange(DataSnapshot snapshot) {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
 
-                    snapshot.getChildren().forEach(i -> {
-                        T item = i.getValue(c);
-                        item.setKey(i.getKey());
-                        waiter.getList().add(item);
-                    });
-                    waiter.respond();
-                }
+                snapshot.getChildren().forEach(i -> {
+                    T item = i.getValue(c);
+                    item.setKey(i.getKey());
+                    waiter.getList().add(item);
+                });
+                waiter.respond();
+            }
 
-                @Override
-                public void onCancelled(DatabaseError error) {
-                    System.out.println("Query is Cancelled");
-                }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.out.println("Query is Cancelled");
+            }
+        });
 
-            });
+        waiter.waitRespond();
 
-            waiter.waitRespond();
-
-            return waiter.getList();
-        }
+        return waiter.getList();
     }
 
     private void updateCurrentUserBookings() {
@@ -128,4 +132,45 @@ public class FirebaseRepository {
 
     }
 
+    private class Waiter
+    {
+        private final int TIMEOUT = 10000;
+
+        private final List list;
+        private Object object;
+
+        private final FutureTask<Void> future;
+        private final ExecutorService exec;
+
+        public Waiter() {
+            future = new FutureTask<Void>(() -> null);
+            exec = Executors.newFixedThreadPool(1);
+            list = new ArrayList();
+        }
+
+        public void waitRespond() {
+            try {
+                future.get(TIMEOUT, TimeUnit.MILLISECONDS);
+            } catch (Throwable e) {System.out.println(e);}
+        }
+
+        public void respond() {
+            exec.execute(future);
+        }
+
+        public void setObject(Object object) {
+            this.object = object;
+        }
+
+        public List getList() {
+            return list;
+        }
+
+        public Object getObject() {
+            return object;
+        }
+    }
+
+
 }
+
