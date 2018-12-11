c9b35fc5a53ecb5427e90aee67188b307bb9d12f
Dominik Wiecek <domis.wiecek@gmail.com>
2018-12-11 14:21:39 +0000

    uofsd

--- app/src/main/java/com/example/domis/android_app/activity/MenuActivity.java
+++ app/src/main/java/com/example/domis/android_app/activity/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.domis.android_app.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -10,13 +12,28 @@ import com.example.domis.android_app.R;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private Button bookingsButton;
+    private Button suppButton;
+    private Button logoutButton;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        bookingsButton = findViewById(R.id.bookingButton);
+        suppButton = findViewById(R.id.suppButton);
+        logoutButton = findViewById(R.id.logoutButton);
+        listView = findViewById(R.id.listView);
 
+        suppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MenuActivity.this, SupportActivity.class));
+            }
+        });
 
 
     }
