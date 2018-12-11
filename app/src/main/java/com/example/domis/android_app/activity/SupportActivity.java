b7be13dcf1fba464eb68fcb08bb643831148c75a
Dominik Wiecek <domis.wiecek@gmail.com>
2018-12-11 14:16:11 +0000

    uofsd

--- app/src/main/java/com/example/domis/android_app/activity/SupportActivity.java
+++ app/src/main/java/com/example/domis/android_app/activity/SupportActivity.java
@@ -1,9 +1,11 @@
-package com.example.domis.android_app;
+package com.example.domis.android_app.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.example.domis.android_app.R;
 
 public class SupportActivity extends AppCompatActivity {
 
